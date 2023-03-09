48caae8f5e2a0862dc39e7801267ccbfba321729
Enrico Teotti <e@t.com>
2023-03-09 08:38:09 -0500

    Add top level integration test to test green path for membership_payment_api -> persistance writing to the DB.

--- spec/requests/membership_api_green_path_spec.rb
+++ spec/requests/membership_api_green_path_spec.rb
@@ -10,4 +10,13 @@ RSpec.describe "Green path on membership API", type: :request do
         end      
     end
     
+    # This is a duplicate of the membership_payment_api test. I added it here to show an example of a green path test to ensure the top level app
+    # integrates with its dependency (membership_payment_api) that uses persistance engine (that writes to the DB).
+    describe 'POST /api/wire_payments/create' do
+        it 'returns http success' do
+            post '/api/wire_payments/create'
+
+            expect(response.status).to eq(200)
+        end      
+    end
 end
